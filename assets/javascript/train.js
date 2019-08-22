de15c395aac7a0ba4268d4957495c738a5dfaf02
stephentsoy92 <alanstephentsoy@gmail.com>
2019-08-22 19:16:48 -0400

    functions perfectly

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,13 +1,13 @@
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyC9RdiLNOSjRHrJEhV_aM7xqt5rCCKS4pY",
-    authDomain: "train-app-f3447.firebaseapp.com",
-    databaseURL: "https://train-app-f3447.firebaseio.com",
-    projectId: "train-app-f3447",
-    storageBucket: "train-app-f3447.appspot.com",
-    messagingSenderId: "564976029877",
-    appId: "1:564976029877:web:e5bac2220811679f"
-    };
+    apiKey: "AIzaSyDjeS0NIP8lyi7LCudzvRZUikrydWuEeXs",
+    authDomain: "train-app-fc628.firebaseapp.com",
+    databaseURL: "https://train-app-fc628.firebaseio.com",
+    projectId: "train-app-fc628",
+    storageBucket: "",
+    messagingSenderId: "640289533967",
+    appId: "1:640289533967:web:2553bb5315e575f3"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
@@ -16,8 +16,7 @@ var trainName= "";
 var destination= "";
 var trainTime;
 var frequency;
-// table row count variable
-var row=1;
+
 // firebase variable read or write
 var database = firebase.database();
 
@@ -32,6 +31,7 @@ $("#submit-btn").on("click", function (event)
         trainTime = $("#trainTime").val().trim()
         frequency = $("#frequency").val().trim()
         
+        //Calculate the time of the train
         function nextTrain (trainTime, frequency)
         {
             var tTime = moment(trainTime, "HH:mm")
@@ -45,6 +45,8 @@ $("#submit-btn").on("click", function (event)
             var nextTrain=currentTime.add(minutesToCompleteFrequency,"minutes").format("HH:mm");
             return nextTrain
         }
+        
+        //Calculates the minute difference
         function minutes (trainTime, frequency)
         {
             var tTime = moment(trainTime, "HH:mm")
@@ -53,41 +55,48 @@ $("#submit-btn").on("click", function (event)
             var minutes=differenceInMinutes%frequency;
             return minutes
         }
-        console.log(nextTrain(trainTime,frequency));
-        console.log(minutes(trainTime,frequency));
+        
+        //store the calculation in variables
         var nextArrival = nextTrain(trainTime,frequency)
         var minutesAway = minutes(trainTime,frequency)
-        tableInput(trainName, destination, frequency, nextArrival, minutesAway)
 
+        //set the values for the firebase database
         var trains = {
             trainName: trainName,
             destination: destination,
             trainTime: trainTime,
-            frequency: frequency
+            frequency: frequency,
+            nextArrival: nextArrival,
+            minutesAway: minutesAway,
         }
-        database.ref(/).set(trains)
-        database.ref().on("value", function(snapshot){
-            console.log(snapshot.val())
-        })
+        database.ref().push(trains)
+
+        // Clears all of the text-boxes
+          $("#trainName").val("");
+          $("#destination").val("");
+          $("#trainTime").val("");
+          $("#frequency").val("");
+
+        alert("Train successfully added");
+
         console.log(trainTime)// WORKS to grab user input
     }
     userInput()
 })
 
 //2. push content to the table
+database.ref().on("child_added", function(snapshot){
+    console.log(snapshot.val());
+    var tName = snapshot.val().trainName;
+    var tDestination = snapshot.val().destination;
+    var tFrequency = snapshot.val().frequency;
+    var nextArrival= snapshot.val().nextArrival;
+    var minutesAway= snapshot.val().minutesAway;
+
+tableInput(tName, tDestination, tFrequency, nextArrival, minutesAway)
+})
+
 function tableInput(trainName, destination, frequency, nextArrival, minutesAway)
 {
     $(".tBody").append('<tr><td>'+trainName+'</td><td>'+destination+'</td><td>'+frequency+'</td><td>'+nextArrival+'</td><td>'+minutesAway+'</td></tr>')
 }
-
-
-// function CreatTrain (name, destination, trainTime, frequency)
-// {
-//     this.name=name;
-//     this.destination=destination;
-//     this.trainTime=trainTime;
-//     this.frequency=frequency;
-// }
-
-// var train1 = new CreatTrain ("bob", "orlando", 9, 120)
-// var train2 = new CreatTrain("carmen", "tampa", 12, 60)
